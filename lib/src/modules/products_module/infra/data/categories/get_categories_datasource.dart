f3ec058a66c9276a6c0cefdcd3ef99d7a6ca6f81
Junior <jos.pd2019@gmail.com>
2022-12-21 19:24:38 -0300

    categories infra tests done!

--- lib/src/modules/products_module/infra/data/categories/get_categories_datasource.dart
+++ lib/src/modules/products_module/infra/data/categories/get_categories_datasource.dart
@@ -1,6 +1,6 @@
 import 'package:virtual_shop_project/src/modules/products_module/domain/entities/categories_entity.dart';
 
 abstract class GetCategoriesDatasource {
-  Future<List<CategoryEntity>> getAllProducts(String category);
-  Future<CategoryEntity> getProductById(String category);
+  Future<List<CategoryEntity>>? getAllCategories(String? category);
+  Future<CategoryEntity>? getCategoryById(String? category);
 }
--- lib/src/modules/products_module/infra/repositories/categories_repo/get_categories_repo_impl.dart
+++ lib/src/modules/products_module/infra/repositories/categories_repo/get_categories_repo_impl.dart
@@ -1,19 +1,36 @@
-import 'package:virtual_shop_project/src/modules/products_module/domain/entities/categories_entity.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:dartz/dartz.dart';
+
+import 'package:virtual_shop_project/src/modules/products_module/domain/entities/categories_entity.dart';
 import 'package:virtual_shop_project/src/modules/products_module/errors/product_errors.dart';
+import 'package:virtual_shop_project/src/modules/products_module/infra/data/categories/get_categories_datasource.dart';
 
 import '../../../domain/repositories/iget_categories_repo.dart';
+import '../../../errors/category_error.dart';
 
-class GetCategoriesRepoImpl implements IGetCategoriesRepo{
+class GetCategoriesRepoImpl implements IGetCategoriesRepo {
+  GetCategoriesRepoImpl({required this.categoriesDatasource});
+
+  GetCategoriesDatasource categoriesDatasource;
   @override
-  Future<Either<ProductModuleErrors, List<CategoryEntity>?>>? getAllCategories(String? category) {
-    // TODO: implement getAllCategories
-    throw UnimplementedError();
+  Future<Either<ProductModuleErrors, List<CategoryEntity>?>>? getAllCategories(
+      String? category) async {
+    try {
+      final result = await categoriesDatasource.getAllCategories(category);
+      return Right(result);
+    } catch (e) {
+      return Left(CategoryError());
+    }
   }
 
   @override
-  Future<Either<ProductModuleErrors, CategoryEntity?>>? getCategoryById(String? categoryId) {
-    // TODO: implement getCategoryById
-    throw UnimplementedError();
+  Future<Either<ProductModuleErrors, CategoryEntity?>>? getCategoryById(
+      String? categoryId) async {
+    try {
+      final result = await categoriesDatasource.getCategoryById(categoryId);
+      return Right(result);
+    } catch (e) {
+      return Left(CategoryError());
+    }
   }
 }
