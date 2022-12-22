24faa4c465dc5eda0fadee509370991832976566
Junior <jos.pd2019@gmail.com>
2022-12-22 17:53:59 -0300

    Product Module/category tested

--- lib/src/modules/products_module/external/category_data/firebase_category_datasource.dart
+++ lib/src/modules/products_module/external/category_data/firebase_category_datasource.dart
@@ -1,17 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:virtual_shop/src/modules/products_module/infra/data/categories/get_categories_datasource.dart';
-
 import '../../infra/models/category_model.dart';
 
 class FirebaseCategoryDatabase implements GetCategoriesDatasource {
+  final _database = FirebaseFirestore.instance;
   @override
-  Future<List<CategoryModel>>? getAllCategories(String? category) {
-    /*   final result = await _database.collection('products').get();
-    return result.docs.map((e) => ProductModel.fromMap(e.data())).toList(); */
+  Future<List<CategoryModel>>? getAllCategories(String? category) async {
+    final result = await _database
+        .collection('products')
+        .doc(category)
+        .collection('items')
+        .get();
+    return result.docs.map((e) => CategoryModel.fromMap(e.data())).toList();
   }
 
   @override
-  Future<CategoryModel>? getCategoryById(String? category) {
-    // TODO: implement getCategoryById
-    throw UnimplementedError();
+  Future<CategoryModel>? getCategoryById(String? category) async {
+    final result = _database.collection('products').doc(category);
+    final categoryData = await result.get();
+    return CategoryModel.fromMap(categoryData.data()!);
   }
 }
--- lib/src/modules/products_module/infra/models/category_model.dart
+++ lib/src/modules/products_module/infra/models/category_model.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 // ignore_for_file: overridden_fields, annotate_overrides
+import 'dart:convert';
+
 import 'package:virtual_shop/src/modules/products_module/domain/entities/categories_entity.dart';
 
 class CategoryModel extends CategoryEntity {
@@ -11,4 +14,24 @@ class CategoryModel extends CategoryEntity {
     this.title,
     this.icon,
   });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'title': title,
+      'icon': icon,
+    };
+  }
+
+  factory CategoryModel.fromMap(Map<String, dynamic> map) {
+    return CategoryModel(
+      id: map['id'] != null ? map['id'] as String : null,
+      title: map['title'] != null ? map['title'] as String : null,
+      icon: map['icon'] != null ? map['icon'] as String : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory CategoryModel.fromJson(String source) => CategoryModel.fromMap(json.decode(source) as Map<String, dynamic>);
 }
